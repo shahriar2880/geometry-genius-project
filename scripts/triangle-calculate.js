91bf30bdec4b4944bfc86d77db23323d4699df86
shahriar2880 <shahriarreza436@gmail.com>
2024-08-19 22:31:07 +0600

    get data from the input field of triangle

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,7 @@
 function calculateTriangleArea(){
     console.log('inside function');
+    const trianglrBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = trianglrBaseInput.value;
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
 }
