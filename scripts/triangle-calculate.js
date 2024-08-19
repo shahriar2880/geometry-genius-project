bea30b2e1558521294d6313e854d1e5dffeb76ab
shahriar2880 <shahriarreza436@gmail.com>
2024-08-19 22:43:10 +0600

    calculate area of triangle and show it to user

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -2,6 +2,15 @@ function calculateTriangleArea(){
     console.log('inside function');
     const trianglrBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = trianglrBaseInput.value;
+    const base = parseFloat(triangleBaseText);
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText)
+    
+    const area = 0.5*base*height ;
+    console.log('Area of triangle is:', area);
+
+    //display triangle area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
 }
