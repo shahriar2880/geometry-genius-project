6b21c93eb0ba802ed1de1168ca83bc0df45d51f9
shahriar2880 <shahriarreza436@gmail.com>
2024-08-20 16:47:28 +0600

    calculate rectangle

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,16 @@
 function calculateRectangleArea(){
-    console.log('rec')
+    //console.log('inside function');
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthText);
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText)
+    
+    const area = width*length ;
+    console.log('Area of Rectangle is:', area);
+
+    //display triangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
